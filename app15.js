17865e4915dda56be4f5f2adba964f00ec762651
giiTamara <claro.giovana@escola.pr.gov.br>
2025-10-27 12:06:53 -0300

    up

--- app15.js
+++ app15.js
@@ -1,11 +1,1 @@
-const btn = document.getElementById("trocarBtn");
-const img2 = document.getElementById("img2");
-
-let imagens = ["michael.jpeg", "michael2.jpeg"];
-let index = 0;
-
-btn.addEventListener("click", () => {
-  index = (index + 1) % imagens.length;
-  img2.src = imagens[index];
-});
 
