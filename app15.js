9113d03c7c4d7ae072fe246163be63ff2141ad52
GigicaTM <claro.giovana@escola.pr.gov.br>
2025-11-04 09:29:16 -0300

    up

---
+++ app15.js
@@ -0,0 +1,17 @@
+const imagem = document.querySelectorAll("img");
+let foto_troca = false;
+
+function TrocaFoto(){
+    if (foto_troca === false){
+        for (let i = 0; i <imagem.length; i++){
+            imagem[i].src = `michaeljackson${i + 1}.jpg`;
+        }
+        foto_troca = true;
+    } else {
+        for (let i = 0; i < imagem.length; i++){
+imagem[i].src = `michael.jpeg`;
+        }
+        foto_troca = false;
+    }
+    
+}
